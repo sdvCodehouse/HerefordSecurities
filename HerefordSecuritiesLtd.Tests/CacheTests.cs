837065a4420a3167cd9cdcc23279ce2d657888db
Simon Da Vall <simon@davall.net>
2017-08-22 12:25:45 +0100

    Improved styling on Admin textareas. Added caching service to project. Added tests to projects.

---
+++ HerefordSecuritiesLtd.Tests/CacheTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HerefordSecuritiesLtd.Models;
+using HerefordSecuritiesLtd.Services;
+using NUnit.Framework;
+
+namespace HerefordSecuritiesLtd.Tests
+{
+    [TestFixture]
+    public class CacheTests
+    {
+        [Test]
+        public void SiteCache_WhenKeyAdded_HasKeyIsTrue()
+        {
+            CachingService.InsertToCache("test", DateTime.Now);
+
+            Assert.That(CachingService.HasKey("test"), Is.True);
+        }
+
+        [Test]
+        public void GetFromCache_WhenAddItemToCache_ItemCanBeRetrieved()
+        {
+            CachingService.InsertToCache("test", new IndexViewModel());
+
+            Assert.That(CachingService.GetFromCache<IndexViewModel>("test"), Is.Not.Null);
+            Assert.That(CachingService.GetFromCache<IndexViewModel>("test"), Is.TypeOf<IndexViewModel>());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            CachingService.Cache.Remove("test");
+        }
+    }
+}
---
+++ HerefordSecuritiesLtd/Services/CachingService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Caching;
+
+namespace HerefordSecuritiesLtd.Services
+{
+    public static class CachingService
+    {
+        public static Cache Cache = new Cache();
+
+        public static bool IsActive
+        {
+            // todo set Cache.IsActive in config
+            get { return true; }
+        }
+
+        public static T GetFromCache<T>(string key) where T : class
+        {
+            if (IsActive && HasKey(key))
+            {
+                return Cache.Get(key) as T;
+            }
+            return null;
+        }
+
+        public static void InsertToCache<T>(string key, T itemToCache)
+        {
+            if (!IsActive) return;
+            Cache.Remove(key);
+            // todo add expiry duration to config
+            Cache.Insert(key, itemToCache, null, DateTime.UtcNow.AddDays(1), Cache.NoSlidingExpiration);
+        }
+
+        public static bool HasKey(string key)
+        {
+            return Cache[key] != null;
+        }
+    }
+}
